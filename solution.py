44a0fca312da4fb9d09837c0a5c8a16c34fd9c5a
xw2535 <email@example.com>
2020-09-07 23:29:13 -0400

    "test2"

--- solution.py
+++ solution.py
@@ -14,9 +14,9 @@ def welcome_assignment_answers(question):
     elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
         answer ="Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer =3
+        answer =5
     elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 6         
+        answer = 4         
     return (answer)         
 
 if __name__ == "__main__":
