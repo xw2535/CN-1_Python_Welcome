d85cf0c2dc6fa4f0e88fa0ab58e0aee07469ea1e
Your Name <you@example.com>
2020-09-07 22:13:54 -0400

    python

---
+++ assignment#2.py
@@ -0,0 +1,33 @@
+def welcome_assignment_answers(question):
+    if question == "Are encoding and encryption the same? - Yes/No":
+        answer ="No"
+    elif question ==  "Is it possible to decrypt a message without a key? - Yes/No":
+        answer ="Yes"
+    elif question ==  "Is it possible to decode a message without a key? - Yes/No":
+        answer ="Yes"
+    elif question ==  "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer ="Yes"
+    elif question ==  "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
+        answer ="Yes"
+    elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer ="Yes"
+    elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
+        answer =3
+    elif question ==  "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
+        answer = 6         
+    return (answer)         
+
+if __name__ == "__main__":
+    debug_question = "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number"
+    print(welcome_assignment_answers(debug_question))
+
+    
+
+
+
+ 
+ 
+ 
+ 
+
+
