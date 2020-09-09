e43e14757dc8493cc404486c23a5f4301e6c2bf4
xw2535 <email@example.com>
2020-09-09 19:22:38 -0400

    md5

--- solution.py
+++ solution.py
@@ -1,16 +1,18 @@
+import hashlib
 def welcome_assignment_answers(question):
 
     #test
     if question == "Are encoding and encryption the same? - Yes/No":
         answer ="No"
     elif question ==  "Is it possible to decrypt a message without a key? - Yes/No":
-        answer ="Yes"
+        answer ="No"
     elif question ==  "Is it possible to decode a message without a key? - Yes/No":
-        answer ="Yes"
+        answer ="No"
     elif question ==  "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer ="Yes"
+        answer ="No"
     elif question ==  "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
-        answer ="Yes"
+        result = hashlib.md5(b'NYU Computer Networking')
+        answer=result.digest()
     elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
         answer ="Yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
